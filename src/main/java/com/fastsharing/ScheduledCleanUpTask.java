5236aa2a57c1c4e06624107cd2ed6702127235b8
WIN7 <WIN7@d72bc7ef-92c2-df48-bcba-db27024c1cf1>
2013-08-28 17:36:16 +0000

    Add schedule clean up
    git-svn-id: file:///media/DATA/GoogleDrive/repos/Projects/FastSharingSpringMVC@3 d72bc7ef-92c2-df48-bcba-db27024c1cf1

---
+++ src/main/java/com/fastsharing/ScheduledCleanUpTask.java
@@ -0,0 +1,10 @@
+package com.fastsharing;
+
+public class ScheduledCleanUpTask implements Runnable {
+
+	public void run() {
+		// Clean up database
+		System.out.println("Cleaning up database");
+	}
+
+}

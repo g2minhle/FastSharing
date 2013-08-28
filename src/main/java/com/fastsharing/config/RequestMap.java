b772ee8a9985aad1c020330f41746714a7ada218
WIN7 <WIN7@d72bc7ef-92c2-df48-bcba-db27024c1cf1>
2013-08-28 20:12:21 +0000

    Remove update result mapping
    git-svn-id: file:///media/DATA/GoogleDrive/repos/Projects/FastSharingSpringMVC@10 d72bc7ef-92c2-df48-bcba-db27024c1cf1

--- src/main/java/com/fastsharing/config/RequestMap.java
+++ src/main/java/com/fastsharing/config/RequestMap.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class RequestMap {
 	public static final String UPLOAD_MAP = "/upload/";
-	public static final String UPLOAD_RESULT_MAP = "/upload/{fileIndex}/";
 	public static final String DOWNLOAD_MAP = "/download/";
 	public static final String DOWNLOAD_FILE_MAP = "/download/{fileIndex}/";	
 	public static final String HOW_TO_USE_MAP = "/howto/";

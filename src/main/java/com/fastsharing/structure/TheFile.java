88bfee7d8e2dc569e42c6b3a5eff1ba110d454a4
WIN7 <WIN7@d72bc7ef-92c2-df48-bcba-db27024c1cf1>
2013-08-29 19:30:33 +0000

    Add documentations
    git-svn-id: file:///media/DATA/GoogleDrive/repos/Projects/FastSharingSpringMVC@19 d72bc7ef-92c2-df48-bcba-db27024c1cf1

--- src/main/java/com/fastsharing/structure/TheFile.java
+++ src/main/java/com/fastsharing/structure/TheFile.java
@@ -13,40 +13,80 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 
+/**
+ * The file entity contains the file and meta information
+ * 
+ * @author g2minhle
+ * 
+ */
 @Entity
 @Table(name = "FILE_STORAGE")
 public class TheFile {
 	@Id
 	@GeneratedValue(generator = "increment")
 	@GenericGenerator(name = "increment", strategy = "increment")
+	/**	The ID of the file in the database */
 	private int fileID;
+
+	/** The file name */
 	@Column(name = "FS_FILE_NAME", columnDefinition = "text")
 	private String fileName;
+
+	/** The updated date of the file */
 	@Column(name = "FS_DATE_UPLOADED")
 	private Date uploadTime;
+
+	/** The content of the file */
 	@Column(name = "FS_THE_FILE")
 	private Blob theFile;
 
+	/**
+	 * A default generator for Hibernate database
+	 */
 	public TheFile() {
 		this.fileName = "";
 		this.theFile = null;
 		this.uploadTime = null;
 	}
 
+	/**
+	 * Create a new file instance
+	 * 
+	 * @param fileName
+	 *            The file name
+	 * @param theFile
+	 *            The file content
+	 */
 	public TheFile(String fileName, Blob theFile) {
 		this.fileName = fileName;
 		this.theFile = theFile;
 		this.uploadTime = new Date();
 	}
 
+	/**
+	 * Get the ID in database of the file
+	 * 
+	 * @return The ID in database of the file
+	 */
 	public int getFileID() {
 		return fileID;
 	}
 
+	/**
+	 * Get the name of the file
+	 * 
+	 * @return The name of the file
+	 */
 	public String getFileName() {
 		return fileName;
 	}
 
+	/**
+	 * Get input stream from file content
+	 * 
+	 * @return The input stream from file content
+	 * @throws SQLException
+	 */
 	public InputStream getFileContent() throws SQLException {
 		return theFile.getBinaryStream();
 	}
